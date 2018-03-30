bd9aa30aa5c2e3d6afc81c7b0d58d7f6f8535d1d
Cyril COLINET <cyril.colinet@epitech.eu>
2018-03-30 20:55:17 +0200

    :tea: Update .gitignore

---
+++ src/matchstick.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2018
+** CPE_matchstick_2017
+** File description:
+** matstick functions
+*/
+
+# include "matchstick.h"
+
+int matchstick_main(int ac, char **av)
+{
+	return (0);
+}
